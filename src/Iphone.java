61dfae1ced3360d84ccd2e426ed3c0e2d8395548
Lucas Silva <nascimentolucas786@gmail.com>
2023-09-19 15:22:49 -0300

    Initial commit

---
+++ src/Iphone.java
@@ -0,0 +1,73 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, Navegador {
+
+    private String model;
+    private String number;
+
+    public Iphone(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música...");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música " + musica + " selecionada!!");
+    }
+
+
+    @Override
+    public void ligar(String numero)  {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo ligação...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando correio de voz...");
+    }
+
+
+    @Override
+    public void exibirPagina() {
+        System.out.println("Exibindo página...");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Adicionando nova aba...");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone("Iphone 7 Pro Max");
+
+        iphone.tocar();
+        iphone.atender();
+        iphone.ligar("2199999999");
+
+        iphone.exibirPagina();
+    }
+}
